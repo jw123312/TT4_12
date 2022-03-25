c352497aa57243d487aedd8521402a049116416b
Gerrie Soh <gerrie_97@yahoo.com>
2022-03-25 11:17:04 +0800

    loginscreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import LoginScreen from "../screens/LoginScreen";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          {/* <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <HomeScreen />
+              </PrivateRoute>
+            }
+          /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
