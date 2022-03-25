a71854132ae0b90a18cd440708912dec0413cf9b
Gerrie Soh <gerrie_97@yahoo.com>
2022-03-25 11:36:17 +0800

    add navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { isAuth, handleLogout } from "../utils/helpers";
+
+export default function Navbar({ children }) {
+  const nav = () => (
+    <ul className="nav nav-tabs" style={{ backgroundColor: "#e3f2fd" }}>
+      <Link to="/" className="navbar-brand text-dark">
+        Loan Management
+      </Link>
+      <li className="nav-item">
+        <Link to="/" className="text-dark nav-link">
+          Home
+        </Link>
+      </li>
+      {
+        <li className="nav-item">
+          <Link to="/login" className="text-dark nav-link">
+            Login
+          </Link>
+        </li>
+      }
+
+      {/* {!isAuth() && (
+        <li className="nav-item">
+          <Link to="/login" className="text-dark nav-link">
+            Login
+          </Link>
+        </li>
+      )} */}
+      {
+        <li className="nav-item">
+          <Link to="/newloan" className="text-dark nav-link">
+            Create New Loan
+          </Link>
+        </li>
+      }
+      {
+        <li className="nav-item">
+          <Link to="/profile" className="text-dark nav-link">
+            Profile
+          </Link>
+        </li>
+      }
+
+      {
+        <li className="nav-item">
+          <span
+            className="text-dark nav-link"
+            style={{ cursor: "pointer", color: "black" }}
+            onClick={handleLogout}
+          >
+            Signout
+          </span>
+        </li>
+      }
+
+      {/* {isAuth() && (
+        <li className="nav-item">
+          <span
+            className="text-dark nav-link"
+            style={{ cursor: "pointer", color: "black" }}
+            onClick={handleLogout}
+          >
+            Signout
+          </span>
+        </li>
+      )} */}
+    </ul>
+  );
+
+  return (
+    <React.Fragment>
+      {nav()}
+      <div className="container">{children}</div>
+    </React.Fragment>
+  );
+}
