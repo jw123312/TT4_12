3098cba3895c69ce961b8c16770e160f5304a7f0
Gerrie Soh <gerrie_97@yahoo.com>
2022-03-25 11:37:41 +0800

    add nav

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import LoginScreen from "./screens/LoginScreen";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<LoginScreen />} />
           {/* <Route
